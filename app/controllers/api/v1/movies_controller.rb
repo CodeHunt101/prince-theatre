1f2bd3ca9ca77f4fed43f5c3c60d33ac71a230b6
Harold Torres Marino <haroldtm55@gmail.com>
2022-02-23 01:03:29 +1100

    Now movies controller renders the movie info as movie cards from the Movie model. Routes were modified accordingly

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,15 +1,8 @@
 class Api::V1::MoviesController < ApplicationController
-  def render_filmworld_movies
-    movies = GetMovies.get_movies('filmworld')
+  def render_movie_cards
+    movie_cards = Movie.movie_cards
     render json: {
-      response: movies
-    }
-  end
-
-  def render_cinemaworld_movies
-    movies = GetMovies.get_movies('cinemaworld')
-    render json: {
-      response: movies
+      response: movie_cards
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get "/filmworld", to: "movies#render_filmworld_movies"
-      get "/cinemaworld", to: "movies#render_cinemaworld_movies"
+      get "/movie_cards", to: "movies#render_movie_cards"
     end
   end
 end
