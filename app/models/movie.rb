69ed957c282f3f0edf516ef0f0fa634728e749d9
Harold Torres Marino <haroldtm55@gmail.com>
2022-02-23 00:34:16 +1100

    Added Movie model that initially handles the logic for retrieving an array containing the movies info from both service providers.

---
+++ app/models/movie.rb
@@ -0,0 +1,42 @@
+class Movie < ApplicationRecord
+  # ActionController::Base.helpers.number_to_currency
+  
+  def self.movie_cards
+    # include ActionView::Helpers::NumberHelper
+    # Since both streaming providers share the same information (except prices) and also the information of each one has the same order, 
+    # the prices of each streaming provider's movie share the same indexes.
+
+    # I mapped one of the streaming providers where I slightly modified the price keys by using a hash with the price of 
+    # the movie of that provider.
+    
+    movies_info_without_cinemaworld_prices = GetMovies.get_movies("filmworld")[:Movies].map do |movie|
+      {
+        id: movie[:ID],
+        title: movie[:Title],
+        poster: movie[:Poster],
+        actors: movie[:Actors],
+        price: {
+          filmworld: self.number_to_currency(movie[:Price])
+        }
+      }
+    end
+
+    # Then I mapped the other streaming provider by only returning an array of prices and
+    # assigning them to a new key that will be the prices of the other streaming provider
+
+    cinemaworld_prices = GetMovies.get_movies("cinemaworld")[:Movies].map do |movie|
+      self.number_to_currency(movie[:Price])
+    end
+
+    movie_cards = movies_info_without_cinemaworld_prices.each_with_index do |movie, index|
+      movie[:price][:cinemaworld] = cinemaworld_prices[index]
+    end
+  end
+
+  private
+  
+  def self.number_to_currency(number)
+    ActionController::Base.helpers.number_to_currency(number)
+  end
+
+end
