da82e694e3fd57045344ed9c3a6aa89eda6e4b43
Harold Torres Marino <haroldtm55@gmail.com>
2022-02-23 19:50:55 +1100

    Installed RSpec for testing Rails models

---
+++ spec/models/get_movies_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe GetMovies, type: :model do
+  # it "has working tests" do
+  #   test_result = GetMovies.test_tests
+  #   expect(test_result).to eq("unit tests working")
+  # end
+end
