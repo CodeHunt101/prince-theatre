39f0bca66744b42d353fe6036b16c59e00528ffa
Harold Torres Marino <haroldtm55@gmail.com>
2022-02-23 20:44:39 +1100

    Added RSpec tests for Movie and GetMovies models to ensure their methods return the expected values

--- spec/models/get_movies_spec.rb
+++ spec/models/get_movies_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe GetMovies, type: :model do
-  # it "has working tests" do
-  #   test_result = GetMovies.test_tests
-  #   expect(test_result).to eq("unit tests working")
-  # end
+  it "GETs the movies from Film World endpoints as JSON and handles errors" do
+    10.times do
+      filmworld_response = GetMovies.get_movies("filmworld")
+      expect(!!filmworld_response[:message]).to eq(false)
+      expect(filmworld_response[:Provider]).to eq("Film World")
+      expect(!!filmworld_response[:Movies]).to eq(true)
+      expect(filmworld_response[:Movies].size).to be > 0
+    end  
+  end
+
+  it "GETs the movies from Cinema World endpoints as JSON and handles errors" do
+     
+    10.times do
+      cinemaworld_response = GetMovies.get_movies("cinemaworld")
+      expect(!!cinemaworld_response[:message]).to eq(false)
+      expect(cinemaworld_response[:Provider]).to eq("Cinema World")
+      expect(!!cinemaworld_response[:Movies]).to eq(true)
+      expect(cinemaworld_response[:Movies].size).to be > 0
+    end
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,8 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  # it "has working tests" do
-  #   test_result = Movie.test_tests
-  #   expect(test_result).to eq("unit tests working")
-  # end
+  it "Converts numbers (integers and floats) to currency" do
+    expect(Movie.number_to_currency(1000)).to eq("$1,000.00") 
+    expect(Movie.number_to_currency(0)).to eq("$0.00")
+    expect(Movie.number_to_currency(20.10)).to eq("$20.10") 
+    expect(Movie.number_to_currency(1000000)).to eq("$1,000,000.00")
+  end
+
+  it "Returns the Movie Cards that displays all the movies information plus the prices of both streaming providers in number and currency formats" do
+    movie_cards = Movie.movie_cards
+    expect(movie_cards.class).to eq(Array)
+    expect(movie_cards.size).to eq(11)
+    expect(movie_cards.first.keys).to eq([:id, :title, :poster, :actors, :price])
+    expect(movie_cards.first[:price].keys).to eq([:filmworld, :cinemaworld])
+    expect(movie_cards.first[:id]).to eq("fw2488496")
+    expect(movie_cards.last[:id]).to eq("fw0121766")
+    expect(movie_cards.first[:price][:filmworld][:number]).to eq(25)
+    expect(movie_cards.first[:price][:cinemaworld][:number]).to eq(24.7)
+  end
 end
